7d0527394b7959bdfb2fd3e37063541ea54bd328
aj243 <ayush243@yahoo.co.in>
2016-06-23 15:59:41 +0530

    unwanted files removed

--- app/assets/javascripts/posts.js
+++
@@ -1,7 +0,0 @@
-count = 0
-source = new EventSource("users/events");
-source.addEventListener("message", function(response) {
-	console.log(response);
-	count = response.data;
-	$('#post_field').val(count);
-});
